a4fb911f8d3ab9c46f05a78a8b06f3dc68926a64
Nisse Knudsen <nisse.knudsen@gmail.com>
2025-07-05 14:11:37 -0700

    Updated to latest v1 manifest added provider

--- app/main.py
+++ app/main.py
@@ -1,19 +1,47 @@
 import logging
+import threading
+from threading import Thread
 
+import zenoh
+from make87.encodings import ProtobufEncoder
+from make87.interfaces.zenoh import ZenohInterface
 from make87_messages.image.compressed.image_jpeg_pb2 import ImageJPEG
 from make87_messages.core.header_pb2 import Header
 import make87
 import cv2
 
+last_image_lock = threading.Lock()
+last_image: bytes
 
-def main():
-    make87.initialize()
 
-    topic = make87.get_publisher(name="IMAGE_DATA", message_type=ImageJPEG)
+def get_last_camera_image() -> bytes:
+    """Get the latest camera image as bytes."""
+    with last_image_lock:
+        if last_image is not None:
+            return last_image
+        else:
+            return b""
+
+
+def handle_get_last_camera_image(query: zenoh.Query):
+    img_msg = get_last_camera_image()
+
+    header = Header(entity_path="/camera")
+    header.timestamp.GetCurrentTime()
+    img_msg = ImageJPEG(data=img_msg, header=header)
+
+    message_encoded = ProtobufEncoder(message_type=ImageJPEG).encode(img_msg)
+    query.reply(key_expr=query.key_expr, payload=message_encoded)
+
+
+def publish_camera_image(config: make87.config.ApplicationConfig):
+    global last_image
+
+    zenoh_interface = ZenohInterface(name="zenoh-client", make87_config=config)
+
+    topic = zenoh_interface.get_publisher(name="IMAGE")
 
     cap = cv2.VideoCapture(make87.resolve_peripheral_name("CAMERA"))
-    camera_entity_name = make87.get_config_value("CAMERA_ENTITY_NAME", "camera", str)
-    verbose = make87.get_config_value("VERBOSE", False, bool)
 
     while True:
         ret, frame = cap.read()
@@ -28,12 +56,24 @@ def main():
 
         frame_jpeg_bytes = frame_jpeg.tobytes()
 
-        message = ImageJPEG(
-            data=frame_jpeg_bytes, header=make87.create_header(Header, entity_path=f"/{camera_entity_name}")
-        )
-        topic.publish(message)
-        if verbose:
-            print(f"Published JPEG with hash: {hash(frame_jpeg_bytes)}")
+        header = Header(entity_path="/camera")
+        message = ImageJPEG(data=frame_jpeg_bytes, header=header)
+        with last_image_lock:
+            last_image = message.data
+        payload = ProtobufEncoder(message_type=ImageJPEG).encode(message)
+        topic.put(payload=payload)
+
+
+def main():
+    config = make87.config.load_config_from_env()
+
+    camera_thread = Thread(target=publish_camera_image, args=(config,))
+    camera_thread.start()
+
+    zenoh_interface = ZenohInterface(name="zenoh-client", make87_config=config)
+    image_provider = zenoh_interface.get_provider(name="GET_CAMERA_IMAGE", handler=handle_get_last_camera_image)
+
+    camera_thread.join()
 
 
 if __name__ == "__main__":
